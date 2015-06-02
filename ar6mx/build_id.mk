9f0fc7f1954f545090695f822da96deec36846db
Frodo Lai <frodo_lai@bcmcom.com>
2015-06-02 12:46:20 -0700

    MA-6028 "ro.build.version.incremental" should be "20150123"
    Update BUILD_ID to rc2
    Update internal release date to 20150123
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- ar6mx/build_id.mk
+++ ar6mx/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.0
-export BUILD_NUMBER=20140917
+export BUILD_ID=2.0.0-rc2
+export BUILD_NUMBER=20150123
