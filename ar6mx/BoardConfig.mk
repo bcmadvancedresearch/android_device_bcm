3a8e612a77a9f14c63c5018eff72584bb032fe28
Moses Yang <moses_yang@bcmcom.com>
2015-06-12 16:04:48 -0700

    Update board files and structure to 5.0.0 format

--- ar6mx/BoardConfig.mk
+++ ar6mx/BoardConfig.mk
@@ -4,45 +4,133 @@
 
 include device/fsl/imx6/soc/imx6dq.mk
 include device/bcm/ar6mx/build_id.mk
-include device/bcm/ar6mx/AR6MXBoardConfigComm.mk
+include device/fsl/imx6/BoardConfigCommon.mk
 include device/fsl-proprietary/gpu-viv/fsl-gpu.mk
+# AR6MX_ default target for EXT4
+BUILD_TARGET_FS = ext4
+BUILD_TARGET_DEVICE = sd
+
+include device/fsl/imx6/imx6_target_fs.mk
+
+#ifeq ($(BUILD_TARGET_DEVICE),sd)
+ADDITIONAL_BUILD_PROPERTIES += \
+                        ro.boot.storage_type=sd
+#ifneq ($(BUILD_TARGET_FS),f2fs)
+TARGET_RECOVERY_FSTAB = device/bcm/ar6mx/fstab.bcm
+# build for ext4
+PRODUCT_COPY_FILES +=	\
+	device/bcm/ar6mx/fstab.bcm:root/fstab.freescale
+#else
+#TARGET_RECOVERY_FSTAB = device/bcm/ar6mx/fstab_sd-f2fs.freescale
+# build for f2fs
+#PRODUCT_COPY_FILES +=	\
+	device/bcm/ar6mx/fstab_sd-f2fs.freescale:root/fstab.freescale
+#endif # BUILD_TARGET_FS
+#else
+#ADDITIONAL_BUILD_PROPERTIES += \
+                        ro.boot.storage_type=emmc
+#ifneq ($(BUILD_TARGET_FS),f2fs)
+#TARGET_RECOVERY_FSTAB = device/bcm/ar6mx/fstab.freescale
+# build for ext4
+#PRODUCT_COPY_FILES +=	\
+	device/bcm/ar6mx/fstab.freescale:root/fstab.freescale
+#else
+#TARGET_RECOVERY_FSTAB = device/bcm/ar6mx/fstab-f2fs.freescale
+# build for f2fs
+#PRODUCT_COPY_FILES +=	\
+	device/bcm/ar6mx/fstab-f2fs.freescale:root/fstab.freescale
+#endif # BUILD_TARGET_FS
+#endif # BUILD_TARGET_DEVICE
+
+
+TARGET_BOOTLOADER_BOARD_NAME := AR6MX
+PRODUCT_MODEL := AR6MX
+
+# UNITE is a virtual device support both atheros and realtek wifi(ar6103 and rtl8723as)
+BOARD_WLAN_DEVICE            := UNITE
+WPA_SUPPLICANT_VERSION       := VER_0_8_UNITE
+TARGET_KERNEL_MODULES        := \
+                                kernel_imx/drivers/net/wireless/rtlwifi/rtl8723as/8723as.ko:system/lib/modules/8723as.ko \
+                                kernel_imx/net/wireless/cfg80211.ko:system/lib/modules/cfg80211_realtek.ko \
+                                kernel_imx/drivers/net/wireless/rtlwifi/rtl8821as/8821as.ko:system/lib/modules/8821as.ko
+BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
+BOARD_HOSTAPD_DRIVER         := NL80211
+
+BOARD_HOSTAPD_PRIVATE_LIB_QCOM              := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB_QCOM       := lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_PRIVATE_LIB_RTL               := lib_driver_cmd_rtl
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB_RTL        := lib_driver_cmd_rtl
+#for intel vendor
+ifeq ($(BOARD_WLAN_VENDOR),INTEL)
+BOARD_HOSTAPD_PRIVATE_LIB                := private_lib_driver_cmd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd
+WPA_SUPPLICANT_VERSION                   := VER_0_8_X
+HOSTAPD_VERSION                          := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd_intel
+WIFI_DRIVER_MODULE_PATH                  := "/system/lib/modules/iwlagn.ko"
+WIFI_DRIVER_MODULE_NAME                  := "iwlagn"
+WIFI_DRIVER_MODULE_PATH                  ?= auto
+endif
+
+BOARD_MODEM_VENDOR := AMAZON
+
+USE_ATHR_GPS_HARDWARE := true
+USE_QEMU_GPS_HARDWARE := false
+
+#for accelerator sensor, need to define sensor type here
+BOARD_HAS_SENSOR := true
+SENSOR_MMA8451 := true
+
+# for recovery service
+TARGET_SELECT_KEY := 28
+
+# we don't support sparse image.
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+DM_VERITY_RUNTIME_CONFIG := true
+# uncomment below lins if use NAND
+#TARGET_USERIMAGES_USE_UBIFS = true
+
+
+ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
+UBI_ROOT_INI := device/fsl/sabresd_6dq/ubi/ubinize.ini
+TARGET_MKUBIFS_ARGS := -m 4096 -e 516096 -c 4096 -x none
+TARGET_UBIRAW_ARGS := -m 4096 -p 512KiB $(UBI_ROOT_INI)
+endif
+
+ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
+ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
+$(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open in same time, please only choose one target file system image")
+endif
+endif
+
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:off video=mxcfb2:off video=mxcfb3:off vmalloc=400M androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale cma=384M
+
+ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
+#UBI boot command line.
+# Note: this NAND partition table must align with MFGTool's config.
+BOARD_KERNEL_CMDLINE +=  mtdparts=gpmi-nand:16m(bootloader),16m(bootimg),128m(recovery),-(root) gpmi_debug_init ubi.mtd=3
+endif
+
+# atheros 3k BT
+BOARD_USE_AR3K_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/bcm/ar6mx/bluetooth
+
+USE_ION_ALLOCATOR := false
+USE_GPU_ALLOCATOR := true
 
 # camera hal v2
 IMX_CAMERA_HAL_V2 := true
 
-BOARD_HAVE_USB_CAMERA := true
+#define consumer IR HAL support
+IMX6_CONSUMER_IR_HAL := true
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M fb0base=0x27b00000 vmalloc=400M androidboot.console=ttymxc0
-
-
-ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
-#UBI boot command line.
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M fb0base=0x27b00000 vmalloc=400M androidboot.console=ttymxc0  mtdparts=gpmi-nand:20m(bootloader),20m(bootimg),20m(recovery),-(root) gpmi_debug_init ubi.mtd=3
-endif
-
-#TARGET_KERNEL_DEFCONF := ar6mx_android_defconfig
-TARGET_KERNEL_DEFCONF := imx_v7_android_defconfig
-USE_ION_ALLOCATOR := false
-USE_GPU_ALLOCATOR := true
 TARGET_BOOTLOADER_CONFIG := 6q:ar6mxqandroid_config 6dl:ar6mxdlandroid_config 6solo:ar6mxsandroid_config
 TARGET_BOARD_DTS_CONFIG := 6q:imx6q-ar6mx.dtb 6dl:imx6dl-ar6mx.dtb
 
-# Filesystem and partitioning
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE	:= 512M
-BOARD_USERDATAIMAGE_PARTITION_SIZE	:= 536870912
-BOARD_CACHEIMAGE_PARTITION_SIZE		:= 268435456
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE	:= ext4
-BOARD_FLASH_BLOCK_SIZE 			:= 4096
-TARGET_USERIMAGES_USE_EXT4		:= true
-TARGET_USERIMAGES_SPARSE_EXT_DISABLED	:= true
-
-# System
-TARGET_NO_RECOVERY			:= false
-TARGET_PROVIDES_INIT_RC			:= true
-TARGET_RECOVERY_FSTAB = device/bcm/ar6mx/fstab.bcm
+TARGET_KERNEL_DEFCONF := imx_v7_android_defconfig
 
 BOARD_SEPOLICY_DIRS := \
-       device/bcm/ar6mx/sepolicy
+       device/bcm/ar6mx/sepolicy 
 
 BOARD_SEPOLICY_UNION := \
        domain.te \
--- ar6mx/ar6mx.mk
+++ ar6mx/ar6mx.mk
@@ -4,28 +4,42 @@
 $(call inherit-product, device/fsl/imx6/imx6.mk)
 $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 
+#ifneq ($(wildcard device/bcm/ar6mx/fstab_nand.freescale),)
+#$(shell touch device/bcm/ar6mx/fstab_nand.freescale)
+#endif
+
+#ifneq ($(wildcard device/bcm/ar6mx/fstab.freescale),)
+#$(shell touch device/bcm/ar6mx/fstab.freescale)
+#endif
+
 # Overrides
 PRODUCT_NAME := ar6mx
 PRODUCT_DEVICE := ar6mx
-PRODUCT_BRAND := bcm
-PRODUCT_MANUFACTURER := bcm
 
 PRODUCT_COPY_FILES += \
 	device/bcm/ar6mx/required_hardware.xml:system/etc/permissions/required_hardware.xml \
-	device/bcm/ar6mx/init.rc:root/init.freescale.rc \
-	device/bcm/ar6mx/fstab.bcm:root/fstab.freescale \
-	device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh \
-	device/bcm/ar6mx/audio_policy.conf:system/etc/audio_policy.conf \
-	device/bcm/ar6mx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	device/bcm/EETI/eGalaxTouch_VirtualDevice.idc:system/usr/idc/eGalaxTouch_VirtualDevice.idc \
+        device/bcm/ar6mx/init.rc:root/init.freescale.rc \
+        device/bcm/ar6mx/gpsreset.sh:system/etc/gpsreset.sh \
+        device/bcm/ar6mx/audio_policy.conf:system/etc/audio_policy.conf \
+        device/bcm/ar6mx/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+        device/bcm/EETI/eGalaxTouch_VirtualDevice.idc:system/usr/idc/eGalaxTouch_VirtualDevice.idc \
         device/bcm/EETI/eGTouchA.ini:data/eGTouchA.ini \
         device/bcm/EETI/eGTouchD:system/bin/eGTouchD \
         device/bcm/init.superuser.rc:root/init.superuser.rc \
-	device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh
+        device/bcm/ar6mx/load_wifi_module.sh:system/etc/load_wifi_module.sh
 
 PRODUCT_COPY_FILES +=	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
+# setup dm-verity configs.
+#ifneq ($(BUILD_TARGET_DEVICE),sd)
+# PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk3p5
+# $(call inherit-product, build/target/product/verity.mk)
+#else 
+ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk2p5
+ $(call inherit-product, build/target/product/verity.mk)
+
+#endif
 
 # GPU files
 
@@ -36,40 +50,37 @@ PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi
 
 PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.faketouch.xml:system/etc/permissions/android.hardware.faketouch.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+	device/bcm/ar6mx/required_hardware.xml:system/etc/permissions/required_hardware.xml
 
-# for PDK build, include only when the dir exists
-# too early to use $(TARGET_BUILD_PDK)
-ifneq ($(wildcard packages/wallpapers/LivePicker),)
-PRODUCT_COPY_FILES += \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
-endif
-
-PRODUCT_PACKAGES += AudioRoute
+#PRODUCT_PACKAGES += AudioRoute
 
 # for Compat driver
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 	device/bcm/ar6mx/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
 
 # include firmware binaries for Wifi adapters
-$(call inherit-product-if-exists, vendor/intel/hardware/wlan/iwl-firmware/iwlwifi.mk)
+#$(call inherit-product-if-exists, vendor/intel/hardware/wlan/iwl-firmware/iwlwifi.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += \
+#PRODUCT_PROPERTY_OVERRIDES += \
         wifi.interface=wlan0
 
 #SUPERUSER_PACKAGE := com.bcm.superuser
 
-SUPERUSER_EMBEDDED := true
+#SUPERUSER_EMBEDDED := true
 
-PRODUCT_PACKAGES += IWLWIFI \
-										su \
+#PRODUCT_PACKAGES += IWLWIFI \
+#										su \
 										Superuser
