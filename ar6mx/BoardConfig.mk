2826f597df1375c3e5a975fb08f7bd5f06d98d89
Moses Yang <moses_yang@bcmcom.com>
2015-07-21 18:59:28 -0700

    AR6MX: add WIFI support

--- ar6mx/BoardConfig.mk
+++ ar6mx/BoardConfig.mk
@@ -46,31 +46,15 @@ PRODUCT_COPY_FILES +=	\
 TARGET_BOOTLOADER_BOARD_NAME := AR6MX
 PRODUCT_MODEL := AR6MX
 
-# UNITE is a virtual device support both atheros and realtek wifi(ar6103 and rtl8723as)
-BOARD_WLAN_DEVICE            := UNITE
-WPA_SUPPLICANT_VERSION       := VER_0_8_UNITE
-TARGET_KERNEL_MODULES        := \
-                                kernel_imx/drivers/net/wireless/rtlwifi/rtl8723as/8723as.ko:system/lib/modules/8723as.ko \
-                                kernel_imx/net/wireless/cfg80211.ko:system/lib/modules/cfg80211_realtek.ko \
-                                kernel_imx/drivers/net/wireless/rtlwifi/rtl8821as/8821as.ko:system/lib/modules/8821as.ko
 BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
 BOARD_HOSTAPD_DRIVER         := NL80211
 
-BOARD_HOSTAPD_PRIVATE_LIB_QCOM              := lib_driver_cmd_qcwcn
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB_QCOM       := lib_driver_cmd_qcwcn
-BOARD_HOSTAPD_PRIVATE_LIB_RTL               := lib_driver_cmd_rtl
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB_RTL        := lib_driver_cmd_rtl
 #for intel vendor
-ifeq ($(BOARD_WLAN_VENDOR),INTEL)
 BOARD_HOSTAPD_PRIVATE_LIB                := private_lib_driver_cmd
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd
 WPA_SUPPLICANT_VERSION                   := VER_0_8_X
 HOSTAPD_VERSION                          := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd_intel
-WIFI_DRIVER_MODULE_PATH                  := "/system/lib/modules/iwlagn.ko"
-WIFI_DRIVER_MODULE_NAME                  := "iwlagn"
 WIFI_DRIVER_MODULE_PATH                  ?= auto
-endif
 
 BOARD_MODEM_VENDOR := AMAZON
 
@@ -130,7 +114,7 @@ TARGET_BOARD_DTS_CONFIG := 6q:imx6q-ar6mx.dtb 6dl:imx6dl-ar6mx.dtb
 TARGET_KERNEL_DEFCONF := imx_v7_android_defconfig
 
 BOARD_SEPOLICY_DIRS := \
-       device/bcm/ar6mx/sepolicy 
+       device/bcm/ar6mx/sepolicy
 
 BOARD_SEPOLICY_UNION := \
        domain.te \
